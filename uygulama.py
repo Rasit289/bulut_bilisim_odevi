c085c145263e40dbbc7101425b604cf3a3a31cb7
Rasit289 <82665438+Rasit289@users.noreply.github.com>
2024-01-24 15:58:08 +0300

    giris sayfasi v6

--- uygulama.py
+++ uygulama.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, validators
 
 app = Flask(__name__, template_folder='templates')
+app.config['SECRET_KEY'] = 'gizli_anahtar'
 
 # Kullanıcı veritabanınızı temsil etmek için basit bir sözlük
 user_database = {
@@ -8,19 +11,25 @@ user_database = {
     'kullanici2': 'parola456',
 }
 
-@app.route('/')
+class LoginForm(FlaskForm):
+    username = StringField('Kullanıcı Adı', [validators.DataRequired()])
+    password = PasswordField('Parola', [validators.DataRequired()])
+    submit = SubmitField('Giriş Yap')
+
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('login.html')
+    form = LoginForm()
 
-@app.route('/login', methods=['POST'])
-def login():
-    username = request.form.get('username')
-    password = request.form.get('password')
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
 
-    if username in user_database and user_database[username] == password:
-        return f"Merhaba, {username}! Başarıyla giriş yaptınız."
-    else:
-        return "Hatalı kullanıcı adı veya parola. Tekrar deneyin."
+        if username in user_database and user_database[username] == password:
+            return f"Merhaba, {username}! Başarıyla giriş yaptınız."
+        else:
+            return "Hatalı kullanıcı adı veya parola. Tekrar deneyin."
+
+    return render_template('login.html', form=form)
 
 if __name__ == '__main__':
     app.run()
